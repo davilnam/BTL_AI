e944fc335bb830a1a36692e0ec0fa7c14751dd4e
nam <namtran10a2nt@gmail.com>
2024-05-05 16:23:39 +0700

    complete

---
+++ main.js
@@ -0,0 +1,282 @@
+var board;
+var game = new Chess();
+
+var chooseBestMove = function (depth, game, isMaximisingPlayer) {
+  var newGameMoves = game.moves();
+  var bestMove = -9999;
+  var bestMoveFound;
+
+  for (var i = 0; i < newGameMoves.length; i++) {
+    var newGameMove = newGameMoves[i];
+    game.move(newGameMove);
+    var value = minimax(depth - 1, game, -10000, 10000, !isMaximisingPlayer);
+    game.undo();
+    if (value >= bestMove) {
+      bestMove = value;
+      bestMoveFound = newGameMove;
+    }
+  }
+  return bestMoveFound;
+};
+
+var minimax = function (depth, game, alpha, beta, isMaximisingPlayer) {
+  if (depth === 0) {
+    return -evaluateBoard(game);
+  }
+
+  var newGameMoves = game.moves();
+
+  if (isMaximisingPlayer) {
+    var bestMove = -9999;
+    for (var i = 0; i < newGameMoves.length; i++) {
+      game.move(newGameMoves[i]);
+      bestMove = Math.max(
+        bestMove,
+        minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer)
+      );
+      game.undo();
+      alpha = Math.max(alpha, bestMove);
+      if (beta <= alpha) {
+        return bestMove;
+      }
+    }
+    return bestMove;
+  } else {
+    var bestMove = 9999;
+    for (var i = 0; i < newGameMoves.length; i++) {
+      game.move(newGameMoves[i]);
+      bestMove = Math.min(
+        bestMove,
+        minimax(depth - 1, game, alpha, beta, !isMaximisingPlayer)
+      );
+      game.undo();
+      beta = Math.min(beta, bestMove);
+      if (beta <= alpha) {
+        return bestMove;
+      }
+    }
+    return bestMove;
+  }
+};
+
+function numberToChar(number) {
+  // Chuyển số thành ký tự bằng cách cộng với mã Unicode của ký tự 'a'
+  return String.fromCharCode("a".charCodeAt(0) + number);
+}
+
+var evaluateBoard = function (board) {
+  var totalEvaluation = 0;
+  for (var i = 0; i < 8; i++) {
+    for (var j = 0; j < 8; j++) {
+      let tmp = numberToChar(j);
+      let pos = tmp + (8 - i).toString();
+      totalEvaluation = totalEvaluation + getPieceValue(board.get(pos), i, j);
+    }
+  }
+  return totalEvaluation;
+};
+
+var reverseArray = function (array) {
+  return array.slice().reverse();
+};
+
+var pawnEvalWhite = [
+  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  [5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0, 5.0],
+  [1.0, 1.0, 2.0, 3.0, 3.0, 2.0, 1.0, 1.0],
+  [0.5, 0.5, 1.0, 2.5, 2.5, 1.0, 0.5, 0.5],
+  [0.0, 0.0, 0.0, 2.0, 2.0, 0.0, 0.0, 0.0],
+  [0.5, -0.5, -1.0, 0.0, 0.0, -1.0, -0.5, 0.5],
+  [0.5, 1.0, 1.0, -2.0, -2.0, 1.0, 1.0, 0.5],
+  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+];
+
+var pawnEvalBlack = reverseArray(pawnEvalWhite);
+
+var knightEval = [
+  [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
+  [-4.0, -2.0, 0.0, 0.0, 0.0, 0.0, -2.0, -4.0],
+  [-3.0, 0.0, 1.0, 1.5, 1.5, 1.0, 0.0, -3.0],
+  [-3.0, 0.5, 1.5, 2.0, 2.0, 1.5, 0.5, -3.0],
+  [-3.0, 0.0, 1.5, 2.0, 2.0, 1.5, 0.0, -3.0],
+  [-3.0, 0.5, 1.0, 1.5, 1.5, 1.0, 0.5, -3.0],
+  [-4.0, -2.0, 0.0, 0.5, 0.5, 0.0, -2.0, -4.0],
+  [-5.0, -4.0, -3.0, -3.0, -3.0, -3.0, -4.0, -5.0],
+];
+
+var bishopEvalWhite = [
+  [-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+  [-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0],
+  [-1.0, 0.0, 0.5, 1.0, 1.0, 0.5, 0.0, -1.0],
+  [-1.0, 0.5, 0.5, 1.0, 1.0, 0.5, 0.5, -1.0],
+  [-1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, -1.0],
+  [-1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, -1.0],
+  [-1.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.5, -1.0],
+  [-2.0, -1.0, -1.0, -1.0, -1.0, -1.0, -1.0, -2.0],
+];
+
+var bishopEvalBlack = reverseArray(bishopEvalWhite);
+
+var rookEvalWhite = [
+  [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+  [0.5, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5],
+  [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+  [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+  [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+  [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+  [-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -0.5],
+  [0.0, 0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0],
+];
+
+var rookEvalBlack = reverseArray(rookEvalWhite);
+
+var evalQueen = [
+  [-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+  [-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, -1.0],
+  [-1.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0],
+  [-0.5, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5],
+  [0.0, 0.0, 0.5, 0.5, 0.5, 0.5, 0.0, -0.5],
+  [-1.0, 0.5, 0.5, 0.5, 0.5, 0.5, 0.0, -1.0],
+  [-1.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, -1.0],
+  [-2.0, -1.0, -1.0, -0.5, -0.5, -1.0, -1.0, -2.0],
+];
+
+var kingEvalWhite = [
+  [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+  [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+  [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+  [-3.0, -4.0, -4.0, -5.0, -5.0, -4.0, -4.0, -3.0],
+  [-2.0, -3.0, -3.0, -4.0, -4.0, -3.0, -3.0, -2.0],
+  [-1.0, -2.0, -2.0, -2.0, -2.0, -2.0, -2.0, -1.0],
+  [2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 2.0, 2.0],
+  [2.0, 3.0, 1.0, 0.0, 0.0, 1.0, 3.0, 2.0],
+];
+
+var kingEvalBlack = reverseArray(kingEvalWhite);
+
+var getPieceValue = function (piece, x, y) {
+  if (piece === null) {
+    return 0;
+  }
+  var getAbsoluteValue = function (piece, isWhite, x, y) {
+    if (piece.type === "p") {
+      return 10 + (isWhite ? pawnEvalWhite[y][x] : pawnEvalBlack[y][x]);
+    } else if (piece.type === "r") {
+      return 50 + (isWhite ? rookEvalWhite[y][x] : rookEvalBlack[y][x]);
+    } else if (piece.type === "n") {
+      return 30 + knightEval[y][x];
+    } else if (piece.type === "b") {
+      return 30 + (isWhite ? bishopEvalWhite[y][x] : bishopEvalBlack[y][x]);
+    } else if (piece.type === "q") {
+      return 90 + evalQueen[y][x];
+    } else if (piece.type === "k") {
+      return 900 + (isWhite ? kingEvalWhite[y][x] : kingEvalBlack[y][x]);
+    }
+    throw "Unknown piece type: " + piece.type;
+  };
+
+  var absoluteValue = getAbsoluteValue(piece, piece.color === "w", x, y);
+  return piece.color === "w" ? absoluteValue : -absoluteValue;
+};
+
+/* board visualization and games state handling */
+
+var onDragStart = function (source, piece, position, orientation) {
+  if (
+    game.in_checkmate() === true ||
+    game.in_draw() === true ||
+    piece.search(/^b/) !== -1
+  ) {
+    return false;
+  }
+};
+
+var makeBestMove = function () {
+  var bestMove = getBestMove(game);
+  game.move(bestMove);
+  if (game.in_checkmate()) {
+    if (game.turn() === "w") {
+      alert("Máy chiến thắng");
+    }
+  }
+  board.position(game.fen());
+  var advantage = evaluateBoard(game);
+  var message = "";
+  if (advantage > 0) {
+    message = "Trắng đang lợi thế hơn" + advantage.toFixed(2);
+  } else if (advantage < 0) {
+    message = "đen đang lợi thế hơn" + Math.abs(advantage).toFixed(2);
+  } else {
+    message = "Hòa!";
+  }
+  document.getElementById("message").innerHTML = message;
+};
+
+var getBestMove = function (game) {
+  var depth = document.getElementById("search-depth").value;
+  console.log(depth);
+  var bestMove = chooseBestMove(depth, game, true);
+  return bestMove;
+};
+
+function displayAdvantage() {
+  var advantage = evaluateBoard(game);
+
+  var message = "";
+  if (advantage > 0) {
+    message = "Trắng đang lợi thế hơn" + advantage.toFixed(2);
+  } else if (advantage < 0) {
+    message = "đen đang lợi thế hơn" + Math.abs(advantage).toFixed(2);
+  } else {
+    message = "Hòa!";
+  }
+  document.getElementById("message").innerHTML = message;
+}
+
+var onDrop = function (source, target) {
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: "q",
+  });
+
+  displayAdvantage();
+
+  if (game.in_checkmate()) {
+    if (game.turn() === "b") {
+      alert("Người chơi chiến thắng");
+    } else {
+      alert("Máy chiến thắng");
+    }
+    board.draggable = false;
+  } else if (game.in_draw()) {
+    alert("Hòa!");
+    board.draggable = false;
+  }
+  if (move === null) {
+    return "snapback";
+  }
+
+  window.setTimeout(makeBestMove, 250);
+};
+
+var onSnapEnd = function () {
+  board.position(game.fen());
+};
+
+var cfg = {
+  draggable: true,
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd,
+};
+board = ChessBoard("board", cfg);
+$("#startBtn").on("click", board.start);
+$("#clearBtn").on("click", board.clear);
+
+const undo = document.getElementById("undo");
+undo.addEventListener("click", function () {
+  game.undo();
+  board.position(game.fen());
+  window.setTimeout(displayAdvantage, 250);  
+});
